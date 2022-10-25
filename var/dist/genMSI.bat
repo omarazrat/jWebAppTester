3d4ef3f60e9dde9df5d9175a72232544951cdb0b
Nestor Arias <31902618+omarazrat@users.noreply.github.com>
2022-10-25 09:58:35 -0500

    feat: inicio de instaladores en Windows.

---
+++ var/dist/genMSI.bat
@@ -0,0 +1,13 @@
+title Instalacion WebAppTester
+set "VERSION=0.3.10"
+set "VERSION_FULL=%VERSION%-SNAPSHOT"
+del /Q /S image
+mkdir image
+cd ..\..\WebAppTester
+call mvn -Dmaven.test.skip=true install dependency:copy-dependencies -DoutputDirectory=../var/dist/image
+echo on
+copy target\WebAppTester-%VERSION_FULL%.jar ..\var\dist\image
+cd ..\var\dist
+jpackage --verbose -n jWebAppTester --app-version %VERSION% --description "selenium Web forms Tester" --icon ..\..\WebAppTester\src\main\resources\icons\selenium.ico --vendor "Nestor Arias" --input image --app-content "image\.,drivers\chromedriver_win32\chromedriver.exe,drivers\edgedriver_win64\msedgedriver.exe,drivers\geckodriver-v0.32.0-win64\geckodriver.exe,drivers\operadriver_win64\operadriver.exe" --main-jar WebAppTester-%VERSION_FULL%.jar --win-dir-chooser --win-menu --win-per-user-install --win-shortcut-prompt -t msi
+del /Q /S image
+@pause
