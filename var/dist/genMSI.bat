4d676c29d7c73c70b2d182c4d5015823e0b30e25
Nestor Arias <31902618+omarazrat@users.noreply.github.com>
2022-10-25 21:32:23 -0500

    feat: pequeña correccion al instalador

--- var/dist/genMSI.bat
+++ var/dist/genMSI.bat
@@ -8,6 +8,6 @@ call mvn -Dmaven.test.skip=true install dependency:copy-dependencies -DoutputDir
 echo on
 copy target\WebAppTester-%VERSION_FULL%.jar ..\var\dist\image
 cd ..\var\dist
-jpackage --verbose -n jWebAppTester --app-version %VERSION% --description "selenium Web forms Tester" --icon ..\..\WebAppTester\src\main\resources\icons\selenium.ico --vendor "Nestor Arias" --input image --app-content "image\.,drivers\chromedriver_win32\chromedriver.exe,drivers\edgedriver_win64\msedgedriver.exe,drivers\geckodriver-v0.32.0-win64\geckodriver.exe,drivers\operadriver_win64\operadriver.exe" --main-jar WebAppTester-%VERSION_FULL%.jar --win-dir-chooser --win-menu --win-per-user-install --win-shortcut-prompt -t msi
+jpackage --verbose -n jWebAppTester --app-version %VERSION% --description "selenium Web forms Tester" --icon ..\..\WebAppTester\src\main\resources\icons\selenium.ico --vendor "Nestor Arias" --input image --app-content "drivers\chromedriver_win32\chromedriver.exe,drivers\edgedriver_win64\msedgedriver.exe,drivers\geckodriver-v0.32.0-win64\geckodriver.exe,drivers\operadriver_win64\operadriver.exe" --main-jar WebAppTester-%VERSION_FULL%.jar --win-dir-chooser --win-menu --win-per-user-install --win-shortcut-prompt -t msi
 del /Q /S image
 @pause
